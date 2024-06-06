548080044f2424a4557893ceb80433c639787ff9
pinknightty <rafagrosa@gmail.com>
2024-06-06 19:57:17 -0300

    Implementação do pacote Scanner e uso dele. Executável pelo .bin diretamente do terminal Power Shell

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+         System.out.println("Digite o número da conta: ");
+         int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da agência: ");
+        String numeroAgencia = scanner.next();
+  
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Saldo da conta: ");
+        double saldoConta = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + 
+        ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
     }
 }
